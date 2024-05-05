08d1736617d8b255220c48c37ad40a0210b4a16f
Kossi D. T. Saka <kdtsaka@gmail.com>
2024-05-05 16:51:28 +0200

    Update the navbar. Convert it into a functionaly component and add a link to the github repo

--- docs/lib/navbar.js
+++ docs/lib/navbar.js
@@ -1,17 +1,20 @@
-
-import React from "react";
-
-export default class Nav extends React.Component {
-	render() {
-		return (
-			<nav className="navbar navbar-fixed-top">
-				<div className="container-fluid">
-					<div className="navbar-header">
-						<a className="navbar-brand" href="index.html">React Stockcharts</a>
-						<div id="debug_here"></div>
-					</div>
+export default function Nav() {
+	return (
+		<nav className="navbar navbar-fixed-top">
+			<div className="container-fluid">
+				<div className="navbar-header">
+					<a className="navbar-brand" href="index.html">
+						React Stockcharts
+					</a>
+					<div id="debug_here"></div>
 				</div>
-			</nav>
-		);
-	}
+				<div class="pull-right">
+					<a href="https://github.com/kossidts/react-stockcharts" class="btn">
+						<small>View project on</small>
+						<br /> <span>GitHub</span>
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
