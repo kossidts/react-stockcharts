95af8873e60fc87e5941de280a1e0d806695177f
Kossi D. T. Saka <kdtsaka@gmail.com>
2024-05-23 08:34:20 +0200

    Fix class attributes

--- docs/lib/page/DemoPage.js
+++ docs/lib/page/DemoPage.js
@@ -9,7 +9,6 @@ import Chart from "../charts/CandleStickChartWithDarkTheme";
 import ContentSection from "lib/content-section";
 import Row from "lib/row";
 // import Section from "lib/section";
-// require("stylesheets/re-stock");
 const parseDate = timeParse("%Y-%m-%d");
 
 function DemoPage() {
@@ -40,23 +39,27 @@ function DemoPage() {
 	return (
 		<ContentSection title="">
 			<Row>
-				<div class="jumbotron" style={{ background: "transparent" }}>
-					<div class="container" id="README.md">
-						<div class="row">
-							<div class="col-md-8" id="content" dangerouslySetInnerHTML={{ __html: ReadME }}></div>
-							<div class="col-md-4"></div>
+				<div className="jumbotron" style={{ background: "transparent" }}>
+					<div className="container" id="README.md">
+						<div className="row">
+							<div
+								className="col-md-8"
+								id="content"
+								dangerouslySetInnerHTML={{ __html: ReadME }}
+							></div>
+							<div className="col-md-4"></div>
 						</div>
 					</div>
 				</div>
 
-				<div class="container light">
-					<div class="row">
-						<div class="col-md-12">
-							{/* <a class="btn btn-primary" href="documentation.html" role="button">
+				<div className="container light">
+					<div className="row">
+						<div className="col-md-12">
+							{/* <a className="btn btn-primary" href="documentation.html" role="button">
 								Documentation
 							</a> */}
 							<h3>Click on the chart, zoom with scroll, pan with drag</h3>
-							<div id="chart" class="react-stockchart">
+							<div id="chart" className="react-stockchart">
 								{!chartData && <span>Loading...</span>}
 								{chartData && <Chart data={chartData} type="hybrid" />}
 							</div>
