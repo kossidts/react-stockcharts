d6135290d3976fc665f7be44a2b2158ce0779bd7
Kossi D. T. Saka <kdtsaka@gmail.com>
2024-06-02 23:31:17 +0200

    Gradually adding tests to the package

--- tests/initial.test.jsx
+++
@@ -1,8 +0,0 @@
-import { it, expect, describe } from "vitest";
-import { render, screen } from "@testing-library/react";
-
-describe("Initial test", () => {
-	it("should work", () => {
-		screen.debug();
-	});
-});
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,14 +1,31 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+
+// const __dirname = fileURLToPath(path.dirname(import.meta.url));
 
 export default defineConfig({
 	test: {
 		environment: "jsdom",
+		// setupFiles: ["./tests/setup.ts"],
+		// testMatch: ["./tests/**/*.test.tsx"],
+		// globals: true,
+	},
+	resolve: {
+		alias: {
+			"@/react-stockcharts": path.resolve(import.meta.dirname, "./src"),
+			// "@/react-stockcharts": path.resolve(__dirname, "./src"),
+			// "react-stockcharts": import.meta.resolve("./src"),
+		},
 	},
 
-	plugins: [react({
-		babel: {
-			configFile: true
-		}
-	})]
+	plugins: [
+		react({
+			babel: {
+				configFile: true,
+			},
+		}),
+	],
 });
