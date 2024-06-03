7305d9066133d43a944590cc904905ed12a43bbd
Kossi D. T. Saka <kdtsaka@gmail.com>
2024-06-03 23:59:47 +0200

    Move Row component to functional component

--- docs/lib/row.js
+++ docs/lib/row.js
@@ -1,26 +1,26 @@
-
 import React from "react";
 import PropTypes from "prop-types";
 
-class Row extends React.Component {
-	render() {
-		const anchor = this.props.anchor || this.props.title;
-		const title = this.props.title
-				? <h4><a id={anchor} href={"#" + anchor}>{this.props.title}</a></h4>
-				: null;
+function Row({ title, anchor, children }) {
+	const anchor = anchor || title;
 
-		return (
-			<div className="row">
-				{title}
-				{this.props.children}
-			</div>
-		);
-	}
+	return (
+		<div className="row">
+			{title && (
+				<h4>
+					<a id={anchor} href={`#${anchor}`}>
+						{title}
+					</a>
+				</h4>
+			)}
+			{children}
+		</div>
+	);
 }
 
 Row.propTypes = {
 	title: PropTypes.string,
-	anchor: PropTypes.string
+	anchor: PropTypes.string,
 };
 
 export default Row;
