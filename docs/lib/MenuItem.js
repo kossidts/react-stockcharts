ad80dd74860c791c0835cfc1225170e6aec23047
Kossi D. T. Saka <kdtsaka@gmail.com>
2024-06-05 09:13:45 +0200

    Refactor the menuItem component

--- docs/lib/MenuItem.js
+++ docs/lib/MenuItem.js
@@ -1,28 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-class MenuItem extends React.Component {
-	render() {
-		const className = this.props.current ? "active" : "";
-		return (
-			<li className={`${className} nav-item`.trim()}>
-				<a href={"#/" + this.props.anchor} className="nav-link">
-					{this.props.title}
-				</a>
-			</li>
-		);
-	}
+/**
+ * MenuItem component for navigation menus.
+ *
+ * @param {object} props - Component properties.
+ * @param {boolean} [props.current] - Whether the item is currently active.
+ * @param {string} props.anchor - Anchor link for the menu item. (Required)
+ * @param {string} props.title - Title of the menu item. (Required)
+ * @returns {JSX.Element}
+ */
+function MenuItem({ current = false, anchor, title }) {
+	const className = current ? "active" : "";
+	return (
+		<li className={`${className} nav-item`.trim()}>
+			<a href={"#/" + anchor} className="nav-link">
+				{title}
+			</a>
+		</li>
+	);
 }
 
-MenuItem.propTypes = {
-	current: PropTypes.bool.isRequired,
-	title: PropTypes.string.isRequired,
-	anchor: PropTypes.string.isRequired,
-};
-
-MenuItem.defaultProps = {
-	active: false,
-};
-
-// onClick={this.handleClick}
 export default MenuItem;
